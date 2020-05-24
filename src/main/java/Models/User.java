8065a07aa951d734b3188234a683a000a69aa714
RobertIonita <robertlucianionita@yahoo.com>
2020-05-24 21:04:43 +0300

    SA-1 Implemented user model and added users json "database"

---
+++ src/main/java/Models/User.java
@@ -0,0 +1,57 @@
+package Models;
+
+import java.util.Objects;
+
+public class User {
+    private String username;
+    private String password;
+    private String role;
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        if(!user.username.equals(username)) return false;
+        if(!user.password.equals(password)) return false;
+        return user.role.equals(role);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 31*username.hashCode() + password.hashCode();
+        result = 31*result + role.hashCode();
+        return result;
+    }
+
+}
