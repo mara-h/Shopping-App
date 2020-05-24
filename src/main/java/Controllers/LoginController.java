f12e7802f44eb453b532d0805c2f1c4db283cc9f
RobertIonita <robertlucianionita@yahoo.com>
2020-05-24 20:54:48 +0300

    SA-1 Implemented interface for login

---
+++ src/main/java/Controllers/LoginController.java
@@ -0,0 +1,22 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    public void userLoginButton(){
+
+    }
+
+    @FXML
+    public void adminLoginButton(){
+
+    }
+}
