f5ee7c1b29801a37b64413e6440b333e92456b2d
RobertIonita <robertlucianionita@yahoo.com>
2020-05-24 21:44:30 +0300

    SA-1 Introduced exceptions

--- src/main/java/Controllers/LoginController.java
+++ src/main/java/Controllers/LoginController.java
@@ -1,5 +1,7 @@
 package Controllers;
 
+import Exceptions.InvalidAdmin;
+import Exceptions.InvalidUser;
 import Services.UserService;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
@@ -13,19 +15,27 @@ public class LoginController {
 
     @FXML
     public void userLoginButton(){
-        if(UserService.checkCredentials(usernameTextField.getText(),passwordField.getText()).equals("customer")){
-            //pagina shop
-        }else{
-            //invalid user
+        try {
+            if (UserService.checkCredentials(usernameTextField.getText(), passwordField.getText()).equals("customer")) {
+                //pagina shop
+            } else {
+                throw new InvalidUser();
+            }
+        }catch (Exception e){
+            System.out.println(e);
         }
     }
 
     @FXML
     public void adminLoginButton(){
-        if(UserService.checkCredentials(usernameTextField.getText(),passwordField.getText()).equals("customer")){
-            //pagina shop
-        }else{
-            //invalid admin
+        try{
+            if(UserService.checkCredentials(usernameTextField.getText(),passwordField.getText()).equals("customer")){
+                //pagina shop
+            }else{
+                throw new InvalidAdmin();
+            }
+        }catch (Exception e){
+            System.out.println(e);
         }
     }
 }
