bc08813a960a38a66703299cfd5d4e23806d7bdb
Mara <marahusar@yahoo.com>
2020-05-25 19:21:23 +0300

    SA-4 Implemented administration page

---
+++ src/main/java/Controllers/AdminController.java
@@ -0,0 +1,25 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+
+public class AdminController {
+    @FXML
+    public void addItemButton(){
+
+    }
+
+    @FXML
+    public void editItemButton(){
+
+    }
+
+    @FXML
+    public void deleteItemButton(){
+
+    }
+
+    @FXML
+    public void backButton(){
+
+    }
+}
