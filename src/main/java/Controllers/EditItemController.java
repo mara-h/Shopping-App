ab8ade75960bcc643a88960b3a2c2c95144d7429
RobertIonita <robertlucianionita@yahoo.com>
2020-06-19 12:59:40 +0300

    SA-5 Added function to back button

--- src/main/java/Controllers/EditItemController.java
+++ src/main/java/Controllers/EditItemController.java
@@ -1,8 +1,12 @@
 package Controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class EditItemController {
     @FXML
@@ -12,7 +16,15 @@ public class EditItemController {
 
     @FXML
     public void backButton(){
-
+        try{
+            Stage primaryStage = (Stage)label.getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("StoreManagement.fxml"));
+            primaryStage.setTitle("Administration Page");
+            primaryStage.setScene(new Scene(root,600,600));
+            primaryStage.show();
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 
     @FXML
